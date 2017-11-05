bd81ffb2fd5aaccc37c2c790df23bcfbcce56306
Abhirup Dey <www.abhirup@gmail.com>
2017-11-05 21:12:07 +0530

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<?php 
+	include_once 'header.php' ;
+?>
+	<section class="main-container">
+		<div class="main-wrapper">
+			<h3> Agricultural Heavy Machinary Renting and Sales managment system</h3>
+					
+		</div>
+	</section>
+
+</body>
+	
+</html>
+
+ 
